12a87281f5dd5d6cc6a64236ff6e5532a2d13313
Jumpei Arima <arijun0307@gmail.com>
2019-05-14 21:53:57 +0900

    update

--- state_lattice_planner/src/motion_planner.cpp
+++ state_lattice_planner/src/motion_planner.cpp
@@ -5,198 +5,89 @@
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <std_msgs/Float64.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Int16.h>
-#include <nav_msgs/Odometry.h>
 
 using namespace std;
 
 const float Vmax=0.9;
 const float ANGULARmax=1.0; 
-const float THRESH_ANG=0.17; //10[deg]
 
 ros::Publisher vel_pub;
 int time_count=0;
-bool v_a_flag=false, cheat_flag=false, target_flag=false, odom_flag=false;
-bool intersection=false;
-geometry_msgs::Twist cheat_vel;
+bool v_a_flag=false, joy_flag=false;
+geometry_msgs::Twist joy_vel;
 boost::mutex v_array_mutex_;
 trajectory_generation::VelocityArray g_v_array; 
-float target=0.0;
-float robot_yaw=0.0;
-int mode=0;
+bool stop_flag=false;
 
-float get_yaw(geometry_msgs::Quaternion q)
-{
-	double r,p,y;
-	tf::Quaternion quat(q.x,q.y,q.z,q.w);
-	tf::Matrix3x3(quat).getRPY(r,p,y);
-	return y;
-}
-
-float normalize(float z)
-{
-  return atan2(sin(z),cos(z));
-}
-float angle_diff(float a, float b)
-{
-  float d1, d2;
-  a = normalize(a);
-  b = normalize(b);
-  d1 = a-b;
-  d2 = 2*M_PI - fabs(d1);
-  if(d1 > 0)
-    d2 *= -1.0;
-  if(fabs(d1) < fabs(d2))
-    return(d1);
-  else
-    return(d2);
-}
-
-void setStopCommand(geometry_msgs::Twist& cmd_vel)
-{
+void setStopCommand(geometry_msgs::Twist& cmd_vel){
 	cmd_vel.linear.x = 0;
 	cmd_vel.angular.z = 0;
 }
 
-void setTurnCommand(geometry_msgs::Twist& cmd_vel,
-					float& target,
-					float& robo)
-{
-	float turn_angular = 0.2;
-	float turn_dir = 1;
-	if(angle_diff(target, robo)>0) turn_dir=-1;
-	cmd_vel.linear.x = 0;
-	cmd_vel.angular.z = turn_dir * turn_angular;
-	cout<<"now turn"<<endl;
-}
-
-void commandDecision(	trajectory_generation::VelocityArray& v_a,
-						geometry_msgs::Twist& cmd_vel)
-{
-	// float hoge = 0.6*10.0/0.4;
+void commandDecision(trajectory_generation::VelocityArray& v_a,
+						geometry_msgs::Twist& cmd_vel){
 	float hoge = 15;
 	int ind = hoge+time_count;
 	if((int)v_a.vel.size() > ind){
-		// cmd_vel.header.stamp = ros::Time::now();
 		cmd_vel.linear.x = v_a.vel[ind].op_linear;
 		cmd_vel.angular.z = -v_a.vel[ind].op_angular;
 	}else{
 		cmd_vel.linear.x = 0;
 		cmd_vel.angular.z = 0;
 	}
-	//cout<<"lin1 = "<<cmd_vel.linear.x<<"\tang1 = "<<cmd_vel.angular.z<<endl;
 }
 
-void CheatCallback(const geometry_msgs::TwistPtr& msg){
-	cheat_vel.linear.x = msg->linear.x;
-	cheat_vel.angular.z = msg->angular.z;
-	cheat_flag = true;
+void JoyCallback(const geometry_msgs::TwistPtr& msg){
+	joy_vel.linear.x = msg->linear.x;
+	joy_vel.angular.z = msg->angular.z;
+	joy_flag = true;
 }
 
-void TargetCallback(const std_msgs::Float64ConstPtr& msg)
-{
-	target=msg->data;
-	target_flag=true;
-}
-
-void OdomCallback(const nav_msgs::OdometryConstPtr& msg)
-{
-	nav_msgs::Odometry odom = *msg;
-	robot_yaw = get_yaw(odom.pose.pose.orientation);
-	odom_flag=true;
-}
-
-void vArrayCallback(const trajectory_generation::VelocityArrayConstPtr& msg)
-{
+void vArrayCallback(const trajectory_generation::VelocityArrayConstPtr& msg){
 	boost::mutex::scoped_lock(v_array_mutex_);
 	g_v_array=*msg;
 	v_a_flag=true;
 }
 
-void ModeCallback(const std_msgs::Int16ConstPtr& msg)
-{
-  mode = msg->data;
-}
-
-void IntersectionCallback(const std_msgs::BoolConstPtr& msg)
-{
-	if(msg->data){
-		intersection=true;
-	}
+void StopCallback(const std_msgs::BoolConstPtr& msg){
+	stop_flag = msg->data;
 }
 
 void MotionPlanner()
 {
 	ros::NodeHandle n;
-	ros::Subscriber cheat_sub   = n.subscribe("/tinypower/cheat_velocity", 10, CheatCallback);
+	ros::Subscriber joy_sub   = n.subscribe("/joy/vel", 10, JoyCallback);
 	ros::Subscriber v_array_sub = n.subscribe("/plan/velocity_array", 10, vArrayCallback);
-	ros::Subscriber target_sub  = n.subscribe("/target_yaw", 10, TargetCallback);
-	ros::Subscriber odom_sub    = n.subscribe("/lcl", 10, OdomCallback);
-	ros::Subscriber flag_sub    = n.subscribe("/intersection_flag",1,IntersectionCallback);
-	
-	ros::Subscriber mode_sub = n.subscribe("/mode",1,ModeCallback);
+	ros::Subscriber stop_sub = n.subscribe("/stop",1,StopCallback);
 
-	vel_pub = n.advertise<geometry_msgs::Twist>("tinypower/command_velocity", 10);
+	vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 	
 	bool turn_flag = false;
-	int stop_count=0;
 	geometry_msgs::Twist cmd_vel;
 
 	ros::Rate loop_rate(10);
 	while(ros::ok()){
-		if (!cheat_flag && target_flag && odom_flag){ //normal state
+		if (!joy_flag && v_a_flag){ //normal state
 			trajectory_generation::VelocityArray v_array; 
-			//if (mode != 1){
-				if (v_a_flag){
-					boost::mutex::scoped_lock(v_array_mutex_);
-					v_array = g_v_array;
-				}
-				if (v_array.vel.size()){// path is generated
-					cout<<"run"<<endl;
-				}
-				else { // path is not generated
-					cout<<"no path stop"<<endl;
-					stop_count++;
-					cout<< "stop_count" << stop_count <<endl;
-				}
-			//}
-		
+			boost::mutex::scoped_lock(v_array_mutex_);
+			v_array = g_v_array;
 			// set velocity and angular //
-			if (mode == 3){
+			if(stop_flag == true){
 				cout<<"stop"<<endl;
 				setStopCommand(cmd_vel);
 			}
-			else if (intersection || turn_flag || mode == 1){
-				if(fabs(angle_diff(target,robot_yaw))>THRESH_ANG){
-					cout<<"turn"<<endl;
-					setTurnCommand(cmd_vel, target, robot_yaw);
-					stop_count=0;
-					turn_flag = false;
-				}else{
-					cout<<"turn end"<<endl;
-					intersection = false;
-					//commandDecision(v_array, cmd_vel);
-					setStopCommand(cmd_vel);
-					stop_count=0;
-					time_count=0;
-				}
-			}
-			else if (mode == 0){ 
+			else if(stop_flag == false){ 
 				cout<<"normal"<<endl;
 				commandDecision(v_array, cmd_vel);
-				stop_count=0;
 				time_count=0;
 			}
-			else {
+			else{
 				cout<<"unknown"<<endl;
 				setStopCommand(cmd_vel);
-				//commandDecision(v_array, cmd_vel);
-				stop_count=0;
 				time_count=0;
 			}
-			
 			// safety //
 			if (cmd_vel.linear.x>Vmax) cmd_vel.linear.x=Vmax;
 			if (cmd_vel.angular.z>ANGULARmax) cmd_vel.angular.z=ANGULARmax;
@@ -205,21 +96,17 @@ void MotionPlanner()
 			// publish //
 			vel_pub.publish(cmd_vel);
 			
-			cout<<"mode: "<<mode<<endl;
+			cout<<"stop flag: "<<stop_flag<<endl;
 			cout<<"lin = "<<cmd_vel.linear.x<<"\tang = "<<cmd_vel.angular.z<<endl<<endl;
-			
-			turn_flag=false;
 		}
-		else if (cheat_flag){
-			vel_pub.publish(cheat_vel);
-			cheat_flag=false;
+		else if (joy_flag){
+			vel_pub.publish(joy_vel);
+			joy_flag=false;
 		}
 		else { //waiting for callback
 			cout<<"-----------"<<endl;
 			cout<<"v_arr:"<<v_a_flag<<endl;
-			cout<<"target:"<<target_flag<<endl;
-			cout<<"odom_flag:"<<odom_flag<<endl;
-			cout<<"cheat_flag:"<<cheat_flag<<endl;
+			cout<<"joy_flag:"<<joy_flag<<endl;
 			cout<<"-----------"<<endl;
 			cmd_vel.linear.x=0;
 			cmd_vel.angular.z=0;

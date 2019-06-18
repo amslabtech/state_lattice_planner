b7de26c8ac77de89fc72e4db0eff4c15529673c4
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-06-18 15:02:00 +0900

    add subscribe

--- state_lattice_planner/include/state_lattice_planner/state_lattice_planner.h
+++ state_lattice_planner/include/state_lattice_planner/state_lattice_planner.h
@@ -2,24 +2,37 @@
 #define __STATE_LATTICE_PLANNER_H
 
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/PoseStamped.h>
 
 #include <Eigen/Dense>
 
 #include <trajectory_generator/motion_model_diff_drive.h>
 #include <trajectory_generator/trajectory_generator_diff_drive.h>
 
-class StateLatticePlanner 
+class StateLatticePlanner
 {
 public:
     StateLatticePlanner(void);
 
     void process(void);
+    void local_goal_callback(const geometry_msgs::PoseStampedConstPtr&);
+    void local_map_callback(const nav_msgs::OccupancyGridConstPtr&);
 
 private:
     double HZ;
 
     ros::NodeHandle nh;
     ros::NodeHandle local_nh;
+
+    ros::Publisher velocity_pub;
+    ros::Subscriber local_map_sub;
+    ros::Subscriber local_goal_sub;
+    geometry_msgs::PoseStamped local_goal;
+    nav_msgs::OccupancyGrid local_map;
+    bool local_goal_subscribed;
+    bool local_map_updated;
 };
 
 #endif //__STATE_LATTICE_PLANNER_H
--- state_lattice_planner/src/state_lattice_planner.cpp
+++ state_lattice_planner/src/state_lattice_planner.cpp
@@ -4,6 +4,25 @@ StateLatticePlanner::StateLatticePlanner(void)
 :local_nh("~")
 {
     local_nh.param("HZ", HZ, {20});
+
+    velocity_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+    local_goal_sub = nh.subscribe("/local_goal", 1, &StateLatticePlanner::local_goal_callback, this);
+    local_map_sub = nh.subscribe("/local_map", 1, &StateLatticePlanner::local_map_callback, this);
+
+    local_goal_subscribed = false;
+    local_map_updated = false;
+}
+
+void StateLatticePlanner::local_goal_callback(const geometry_msgs::PoseStampedConstPtr& msg)
+{
+    local_goal = *msg;
+    local_goal_subscribed = true;
+}
+
+void StateLatticePlanner::local_map_callback(const nav_msgs::OccupancyGridConstPtr& msg)
+{
+    local_map = *msg;
+    local_map_updated = true;
 }
 
 void StateLatticePlanner::process(void)
@@ -11,7 +30,16 @@ void StateLatticePlanner::process(void)
     ros::Rate loop_rate(HZ);
 
     while(ros::ok()){
+        if(local_goal_subscribed && local_map_updated){
 
+        }else{
+            if(!local_goal_subscribed){
+                std::cout << "waiting for local goal" << std::endl;
+            }
+            if(!local_map_updated){
+                std::cout << "waiting for local map" << std::endl;
+            }
+        }
         loop_rate.sleep();
     }
 }

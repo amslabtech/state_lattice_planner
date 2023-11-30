9c4aaa993f79c22c8251cfbc6627aef44d43c723
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2023-11-30 15:20:27 +0000

    Fix format

--- include/state_lattice_planner/state_lattice_planner_ros.h
+++ include/state_lattice_planner/state_lattice_planner_ros.h
@@ -1,79 +1,89 @@
-#ifndef __STATE_LATTICE_PLANNER_ROS_H
-#define __STATE_LATTICE_PLANNER_ROS_H
+// Copyright 2019 amsl
+#ifndef STATE_LATTICE_PLANNER_STATE_LATTICE_PLANNER_ROS_H
+#define STATE_LATTICE_PLANNER_STATE_LATTICE_PLANNER_ROS_H
 
-#include <ros/ros.h>
+#include <string>
+#include <vector>
+
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/Odometry.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
+#include <ros/ros.h>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
 #include "state_lattice_planner/state_lattice_planner.h"
 
 class StateLatticePlannerROS
 {
 public:
-    StateLatticePlannerROS(void);
+  StateLatticePlannerROS(void);
 
-    void process(void);
-    void local_goal_callback(const geometry_msgs::PoseStampedConstPtr&);
-    void local_map_callback(const nav_msgs::OccupancyGridConstPtr&);
-    void odom_callback(const nav_msgs::OdometryConstPtr&);
-    void target_velocity_callback(const geometry_msgs::TwistConstPtr&);
-    template<typename TYPE>
-    void get_obstacle_map(const nav_msgs::OccupancyGrid&, state_lattice_planner::ObstacleMap<TYPE>&);
+  void process(void);
+  void local_goal_callback(const geometry_msgs::PoseStampedConstPtr&);
+  void local_map_callback(const nav_msgs::OccupancyGridConstPtr&);
+  void odom_callback(const nav_msgs::OdometryConstPtr&);
+  void target_velocity_callback(const geometry_msgs::TwistConstPtr&);
+  template <typename TYPE>
+  void get_obstacle_map(const nav_msgs::OccupancyGrid&,
+                        state_lattice_planner::ObstacleMap<TYPE>&);
 
 protected:
-    void visualize_trajectories(const std::vector<MotionModelDiffDrive::Trajectory>&, const double, const double, const double, const int, const ros::Publisher&);
-    void visualize_trajectory(const MotionModelDiffDrive::Trajectory&, const double, const double, const double, const ros::Publisher&);
+  void
+  visualize_trajectories(const std::vector<MotionModelDiffDrive::Trajectory>&,
+                         const double, const double, const double, const int,
+                         const ros::Publisher&);
+  void visualize_trajectory(const MotionModelDiffDrive::Trajectory&,
+                            const double, const double, const double,
+                            const ros::Publisher&);
 
-    double HZ;
-    std::string ROBOT_FRAME;
-    int N_P;
-    int N_H;
-    int N_S;
-    double MAX_ALPHA;
-    double MAX_PSI;
-    double MAX_ACCELERATION;
-    double TARGET_VELOCITY;
-    std::string LOOKUP_TABLE_FILE_NAME;
-    int MAX_ITERATION;
-    double OPTIMIZATION_TOLERANCE;
-    double MAX_YAWRATE;
-    double MAX_D_YAWRATE;
-    double MAX_WHEEL_ANGULAR_VELOCITY;
-    double WHEEL_RADIUS;
-    double TREAD;
-    double IGNORABLE_OBSTACLE_RANGE;
-    bool VERBOSE;
-    int CONTROL_DELAY;
-    double TURN_DIRECTION_THRESHOLD;
-    bool ENABLE_SHARP_TRAJECTORY;
-    bool ENABLE_CONTROL_SPACE_SAMPLING;
+  double HZ;
+  std::string ROBOT_FRAME;
+  int N_P;
+  int N_H;
+  int N_S;
+  double MAX_ALPHA;
+  double MAX_PSI;
+  double MAX_ACCELERATION;
+  double TARGET_VELOCITY;
+  std::string LOOKUP_TABLE_FILE_NAME;
+  int MAX_ITERATION;
+  double OPTIMIZATION_TOLERANCE;
+  double MAX_YAWRATE;
+  double MAX_D_YAWRATE;
+  double MAX_WHEEL_ANGULAR_VELOCITY;
+  double WHEEL_RADIUS;
+  double TREAD;
+  double IGNORABLE_OBSTACLE_RANGE;
+  bool VERBOSE;
+  int CONTROL_DELAY;
+  double TURN_DIRECTION_THRESHOLD;
+  bool ENABLE_SHARP_TRAJECTORY;
+  bool ENABLE_CONTROL_SPACE_SAMPLING;
 
-    ros::NodeHandle nh;
-    ros::NodeHandle local_nh;
+  ros::NodeHandle nh;
+  ros::NodeHandle local_nh;
 
-    ros::Publisher velocity_pub;
-    ros::Publisher candidate_trajectories_pub;
-    ros::Publisher candidate_trajectories_no_collision_pub;
-    ros::Publisher selected_trajectory_pub;
-    ros::Subscriber local_map_sub;
-    ros::Subscriber local_goal_sub;
-    ros::Subscriber odom_sub;
-    ros::Subscriber target_velocity_sub;
-    tf::TransformListener listener;
-    geometry_msgs::PoseStamped local_goal;
-    nav_msgs::OccupancyGrid local_map;
-    geometry_msgs::Twist current_velocity;
-    bool local_goal_subscribed;
-    bool local_map_updated;
-    bool odom_updated;
+  ros::Publisher velocity_pub;
+  ros::Publisher candidate_trajectories_pub;
+  ros::Publisher candidate_trajectories_no_collision_pub;
+  ros::Publisher selected_trajectory_pub;
+  ros::Subscriber local_map_sub;
+  ros::Subscriber local_goal_sub;
+  ros::Subscriber odom_sub;
+  ros::Subscriber target_velocity_sub;
+  tf::TransformListener listener;
+  geometry_msgs::PoseStamped local_goal;
+  nav_msgs::OccupancyGrid local_map;
+  geometry_msgs::Twist current_velocity;
+  bool local_goal_subscribed;
+  bool local_map_updated;
+  bool odom_updated;
 
-    StateLatticePlanner planner;
+  StateLatticePlanner planner;
 };
 
-#endif //__STATE_LATTICE_PLANNER_ROS_H
+#endif  // STATE_LATTICE_PLANNER_STATE_LATTICE_PLANNER_ROS_H
--- src/trajectory_generator_diff_drive.cpp
+++ src/trajectory_generator_diff_drive.cpp
@@ -1,191 +1,240 @@
+// Copyright 2019 amsl
+
 #include "trajectory_generator/trajectory_generator_diff_drive.h"
 
 TrajectoryGeneratorDiffDrive::TrajectoryGeneratorDiffDrive(void)
 {
-    // default
-    h << 0.05, 0.05, 0.1;
-    MAX_YAWRATE = 1.0;
+  // default
+  h << 0.05, 0.05, 0.1;
+  MAX_YAWRATE = 1.0;
 }
 
-void TrajectoryGeneratorDiffDrive::set_optimization_param(const double dkm, const double dkf, const double dsf)
+void TrajectoryGeneratorDiffDrive::set_optimization_param(const double dkm,
+                                                          const double dkf,
+                                                          const double dsf)
 {
-    h << dkm, dkf, dsf;
+  h << dkm, dkf, dsf;
 }
 
-void TrajectoryGeneratorDiffDrive::set_motion_param(const double max_yawrate, const double max_d_yawrate, const double max_acceleration, const double max_wheel_angular_velocity, const double wheel_radius, const double tread)
+void TrajectoryGeneratorDiffDrive::set_motion_param(
+    const double max_yawrate, const double max_d_yawrate,
+    const double max_acceleration, const double max_wheel_angular_velocity,
+    const double wheel_radius, const double tread)
 {
-    MAX_YAWRATE = max_yawrate;
-    model.set_param(max_yawrate, max_d_yawrate, max_acceleration, max_wheel_angular_velocity, wheel_radius, tread);
+  MAX_YAWRATE = max_yawrate;
+  model.set_param(max_yawrate, max_d_yawrate, max_acceleration,
+                  max_wheel_angular_velocity, wheel_radius, tread);
 }
 
 void TrajectoryGeneratorDiffDrive::set_verbose(bool verbose_)
 {
-    verbose = verbose_;
+  verbose = verbose_;
 }
 
-double TrajectoryGeneratorDiffDrive::generate_optimized_trajectory(const Eigen::Vector3d& goal, const MotionModelDiffDrive::ControlParams& init_control_param, const double dt, const double tolerance, const int max_iteration, MotionModelDiffDrive::ControlParams& output, MotionModelDiffDrive::Trajectory& trajectory)
+double TrajectoryGeneratorDiffDrive::generate_optimized_trajectory(
+    const Eigen::Vector3d& goal,
+    const MotionModelDiffDrive::ControlParams& init_control_param,
+    const double dt, const double tolerance, const int max_iteration,
+    MotionModelDiffDrive::ControlParams& output,
+    MotionModelDiffDrive::Trajectory& trajectory)
 {
-    Eigen::Vector3d cost(1e2, 1e2, 1e2);
-    double last_cost = cost.norm();
+  Eigen::Vector3d cost(1e2, 1e2, 1e2);
+  double last_cost = cost.norm();
 
-    double distance_to_goal = goal.segment(0, 2).norm();
-    // std::cout << "d_goal: " << distance_to_goal << "[m]" << std::endl;
+  double distance_to_goal = goal.segment(0, 2).norm();
+  // std::cout << "d_goal: " << distance_to_goal << "[m]" << std::endl;
 
-    output = init_control_param;
+  output = init_control_param;
 
-    int count = 0;
+  int count = 0;
 
-    Eigen::Matrix3d jacobian;
+  Eigen::Matrix3d jacobian;
 
-    while(1){
-        // std::cout << "---" << std::endl;
-        if(cost.norm() < tolerance){
-            if(verbose){
-                std::cout << "successfully optimized in " << count << " iteration" << std::endl;
-            }
-            break;
-        }else if(count >= max_iteration){
-            if(verbose){
-                std::cout << "cannot optimize trajectory" << std::endl;
-            }
-            return -1;
-        }
-        trajectory.trajectory.clear();
-        trajectory.velocities.clear();
-        trajectory.angular_velocities.clear();
-        model.generate_trajectory(dt, output, trajectory);
-        /*
-        std::cout << "size: " << trajectory.trajectory.size() << std::endl;
-        if(trajectory.trajectory.size() <= 1){
-            std::cout << "failed to generate trajecotry!!!" << std::endl;
-            return -1;
-        }
-        */
-
-        get_jacobian(dt, output, h, jacobian);
-        // std::cout << "j: \n" << jacobian << std::endl;
-        // std::cout << "j^-1: \n" << jacobian.inverse() << std::endl;
-        cost = goal - trajectory.trajectory.back();
-        // Eigen::Vector3d dp = jacobian.inverse() * cost;
-        Eigen::Vector3d dp = jacobian.lu().solve(cost);
-        if(std::isnan(dp(0)) || std::isnan(dp(1)) || std::isnan(dp(2)) || std::isinf(dp(0)) || std::isinf(dp(1)) || std::isinf(dp(2)) || fabs(dp(2)) > distance_to_goal){
-            if(verbose){
-                std::cout << "diverge to infinity!!!" << std::endl;
-            }
-            return -1;
-        }
-        // std::cout << "cost vector: " << cost.transpose() << std::endl;
-        // std::cout << "cost: " << cost.norm() << std::endl;
-        // std::cout << "dp" << dp.transpose() << std::endl;
-        // std::cout << "calculate scale factor" << std::endl;
-        calculate_scale_factor(dt, tolerance, goal, cost, output, trajectory, dp);
-
-        // std::cout << "last state: \n" << trajectory.trajectory.back() << std::endl;
-        // std::cout << "cost vector: \n" << cost << std::endl;
-        // std::cout << "cost: \n" << cost.norm() << std::endl;
-        if(cost.norm() > 100){
-            std::cout << "cost is too large" << std::endl;
-            return -1;
-        }
-        // std::cout << "dp: \n" << dp << std::endl;
-        if((cost.norm() > last_cost) || std::isnan(dp(0)) || std::isnan(dp(1)) || std::isnan(dp(2)) || std::isinf(dp(0)) || std::isinf(dp(1)) || std::isinf(dp(2)) || fabs(dp(2)) > distance_to_goal){
-            if(verbose){
-                std::cout << "diverge to infinity!!!" << std::endl;
-            }
-            return -1;
-        }
-        last_cost = cost.norm();
-
-        output.omega.km += dp(0);
-        output.omega.kf += dp(1);
-        output.omega.sf += dp(2);
-        // output.omega.km = std::min(std::max(output.omega.km, -MAX_YAWRATE), MAX_YAWRATE);
-        // output.omega.kf = std::min(std::max(output.omega.kf, -MAX_YAWRATE), MAX_YAWRATE);
-        // std::cout << "output: " << output.omega.km << ", " << output.omega.kf << ", " << output.omega.sf << std::endl;
-
-        // if(fabsf(output.omega.sf - distance_to_goal) > distance_to_goal * 0.5){
-        //     if(verbose){
-        //         std::cout << "optimization error!!!" << std::endl;
-        //     }
-        //     return -1;
-        // }
-        //std::cout << output.omega.km << ", " << output.omega.kf << ", " << output.omega.sf << std::endl;
-
-        // std::cout << "count: " << count << std::endl;
-        count++;
+  while (1)
+  {
+    // std::cout << "---" << std::endl;
+    if (cost.norm() < tolerance)
+    {
+      if (verbose)
+      {
+        std::cout << "successfully optimized in " << count << " iteration"
+                  << std::endl;
+      }
+      break;
     }
-    //std::cout << "final cost: \n" << cost << std::endl;
-    return cost.norm();
-}
-
-void TrajectoryGeneratorDiffDrive::get_jacobian(const double dt, const MotionModelDiffDrive::ControlParams& control, const Eigen::Vector3d& h, Eigen::Matrix3d& j)
-{
+    else if (count >= max_iteration)
+    {
+      if (verbose)
+      {
+        std::cout << "cannot optimize trajectory" << std::endl;
+      }
+      return -1;
+    }
+    trajectory.trajectory.clear();
+    trajectory.velocities.clear();
+    trajectory.angular_velocities.clear();
+    model.generate_trajectory(dt, output, trajectory);
     /*
-     * h: (dkm, dkf, dsf)
-     */
-    //std::cout << "j start" << std::endl;
-    MotionModelDiffDrive::AngularVelocityParams omega = control.omega;
-    Eigen::Vector3d x0;
-    model.generate_last_state(dt, omega.sf, control.vel, omega.k0, omega.km - h(0), omega.kf, x0);
-    Eigen::Vector3d x1;
-    model.generate_last_state(dt, omega.sf, control.vel, omega.k0, omega.km + h(0), omega.kf, x1);
+    std::cout << "size: " << trajectory.trajectory.size() << std::endl;
+    if(trajectory.trajectory.size() <= 1){
+        std::cout << "failed to generate trajecotry!!!" << std::endl;
+        return -1;
+    }
+    */
 
-    Eigen::Vector3d dx_dkm;
-    dx_dkm << (x1 - x0) / (2.0 * h(0));
+    get_jacobian(dt, output, h, jacobian);
+    // std::cout << "j: \n" << jacobian << std::endl;
+    // std::cout << "j^-1: \n" << jacobian.inverse() << std::endl;
+    cost = goal - trajectory.trajectory.back();
+    // Eigen::Vector3d dp = jacobian.inverse() * cost;
+    Eigen::Vector3d dp = jacobian.lu().solve(cost);
+    if (std::isnan(dp(0)) || std::isnan(dp(1)) || std::isnan(dp(2)) ||
+        std::isinf(dp(0)) || std::isinf(dp(1)) || std::isinf(dp(2)) ||
+        fabs(dp(2)) > distance_to_goal)
+    {
+      if (verbose)
+      {
+        std::cout << "diverge to infinity!!!" << std::endl;
+      }
+      return -1;
+    }
+    // std::cout << "cost vector: " << cost.transpose() << std::endl;
+    // std::cout << "cost: " << cost.norm() << std::endl;
+    // std::cout << "dp" << dp.transpose() << std::endl;
+    // std::cout << "calculate scale factor" << std::endl;
+    calculate_scale_factor(dt, tolerance, goal, cost, output, trajectory, dp);
 
-    model.generate_last_state(dt, omega.sf, control.vel, omega.k0, omega.km, omega.kf - h(1), x0);
-    model.generate_last_state(dt, omega.sf, control.vel, omega.k0, omega.km, omega.kf + h(1), x1);
+    // std::cout << "last state: \n" << trajectory.trajectory.back() <<
+    // std::endl; std::cout << "cost vector: \n" << cost << std::endl; std::cout
+    // << "cost: \n" << cost.norm() << std::endl;
+    if (cost.norm() > 100)
+    {
+      std::cout << "cost is too large" << std::endl;
+      return -1;
+    }
+    // std::cout << "dp: \n" << dp << std::endl;
+    if ((cost.norm() > last_cost) || std::isnan(dp(0)) || std::isnan(dp(1)) ||
+        std::isnan(dp(2)) || std::isinf(dp(0)) || std::isinf(dp(1)) ||
+        std::isinf(dp(2)) || fabs(dp(2)) > distance_to_goal)
+    {
+      if (verbose)
+      {
+        std::cout << "diverge to infinity!!!" << std::endl;
+      }
+      return -1;
+    }
+    last_cost = cost.norm();
 
-    Eigen::Vector3d dx_dkf;
-    dx_dkf << (x1 - x0) / (2.0 * h(1));
+    output.omega.km += dp(0);
+    output.omega.kf += dp(1);
+    output.omega.sf += dp(2);
+    // output.omega.km = std::min(std::max(output.omega.km, -MAX_YAWRATE),
+    // MAX_YAWRATE); output.omega.kf = std::min(std::max(output.omega.kf,
+    // -MAX_YAWRATE), MAX_YAWRATE); std::cout << "output: " << output.omega.km
+    // << ", " << output.omega.kf << ", " << output.omega.sf << std::endl;
 
-    model.generate_last_state(dt, omega.sf - h(2), control.vel, omega.k0, omega.km, omega.kf, x0);
-    model.generate_last_state(dt, omega.sf + h(2), control.vel, omega.k0, omega.km, omega.kf, x1);
+    // if(fabsf(output.omega.sf - distance_to_goal) > distance_to_goal * 0.5){
+    //     if(verbose){
+    //         std::cout << "optimization error!!!" << std::endl;
+    //     }
+    //     return -1;
+    // }
+    // std::cout << output.omega.km << ", " << output.omega.kf << ", " <<
+    // output.omega.sf << std::endl;
 
-    Eigen::Vector3d dx_dsf;
-    dx_dsf << (x1 - x0) / (2.0 * h(2));
-
-    j << dx_dkm(0), dx_dkf(0), dx_dsf(0),
-         dx_dkm(1), dx_dkf(1), dx_dsf(1),
-         dx_dkm(2), dx_dkf(2), dx_dsf(2);
+    // std::cout << "count: " << count << std::endl;
+    count++;
+  }
+  // std::cout << "final cost: \n" << cost << std::endl;
+  return cost.norm();
 }
 
-void TrajectoryGeneratorDiffDrive::calculate_scale_factor(double dt, double tolerance, const Eigen::Vector3d& goal, Eigen::Vector3d& cost, MotionModelDiffDrive::ControlParams& output, MotionModelDiffDrive::Trajectory& trajectory, Eigen::Vector3d& dp)
+void TrajectoryGeneratorDiffDrive::get_jacobian(
+    const double dt, const MotionModelDiffDrive::ControlParams& control,
+    const Eigen::Vector3d& h, Eigen::Matrix3d& j)
 {
-    for(double beta=0.25;beta<=1.5;beta+=0.25){
-        // std::cout << "beta: " << beta << std::endl;
-        if(beta == 1.0f){
-            continue;
-        }
-        Eigen::Vector3d dp_ = beta * dp;
-        MotionModelDiffDrive::Trajectory trajectory_;
-        MotionModelDiffDrive::ControlParams output_ = output;
-        output_.omega.km += dp_(0);
-        output_.omega.kf += dp_(1);
-        output_.omega.sf += dp_(2);
-        // std::cout << "dp_: " << dp_.transpose() << std::endl;
-        // output_.omega.km = std::min(std::max(output_.omega.km, -MAX_YAWRATE), MAX_YAWRATE);
-        // output_.omega.kf = std::min(std::max(output_.omega.kf, -MAX_YAWRATE), MAX_YAWRATE);
-        // std::cout << "output: " << output_.omega.km << ", " << output_.omega.kf << ", " << output_.omega.sf << std::endl;
-        model.generate_trajectory(dt, output_, trajectory_);
-        // std::cout << "size: " << trajectory_.trajectory.size() << std::endl;
-        if(trajectory_.trajectory.size() <= 1){
-            // std::cout << "failed to generate trajecotry!!!" << std::endl;
-            continue;
-        }
-        Eigen::Vector3d cost_ = goal - trajectory_.trajectory.back();
-        // std::cout << cost_.norm() << " vs " << cost.norm() << std::endl;
-        if(cost_.norm() < cost.norm()){
-            cost = cost_;
-            output = output_;
-            trajectory = trajectory_;
-            dp = dp_;
-            // std::cout << "updated!!!" << std::endl;
-        }else{
-            // std::cout << "not updated!!!" << std::endl;
-        }
-        if(cost.norm() < tolerance){
-            return;
-        }
+  /*
+   * h: (dkm, dkf, dsf)
+   */
+  // std::cout << "j start" << std::endl;
+  MotionModelDiffDrive::AngularVelocityParams omega = control.omega;
+  Eigen::Vector3d x0;
+  model.generate_last_state(dt, omega.sf, control.vel, omega.k0,
+                            omega.km - h(0), omega.kf, x0);
+  Eigen::Vector3d x1;
+  model.generate_last_state(dt, omega.sf, control.vel, omega.k0,
+                            omega.km + h(0), omega.kf, x1);
+
+  Eigen::Vector3d dx_dkm;
+  dx_dkm << (x1 - x0) / (2.0 * h(0));
+
+  model.generate_last_state(dt, omega.sf, control.vel, omega.k0, omega.km,
+                            omega.kf - h(1), x0);
+  model.generate_last_state(dt, omega.sf, control.vel, omega.k0, omega.km,
+                            omega.kf + h(1), x1);
+
+  Eigen::Vector3d dx_dkf;
+  dx_dkf << (x1 - x0) / (2.0 * h(1));
+
+  model.generate_last_state(dt, omega.sf - h(2), control.vel, omega.k0,
+                            omega.km, omega.kf, x0);
+  model.generate_last_state(dt, omega.sf + h(2), control.vel, omega.k0,
+                            omega.km, omega.kf, x1);
+
+  Eigen::Vector3d dx_dsf;
+  dx_dsf << (x1 - x0) / (2.0 * h(2));
+
+  j << dx_dkm(0), dx_dkf(0), dx_dsf(0), dx_dkm(1), dx_dkf(1), dx_dsf(1),
+      dx_dkm(2), dx_dkf(2), dx_dsf(2);
+}
+
+void TrajectoryGeneratorDiffDrive::calculate_scale_factor(
+    double dt, double tolerance, const Eigen::Vector3d& goal,
+    Eigen::Vector3d& cost, MotionModelDiffDrive::ControlParams& output,
+    MotionModelDiffDrive::Trajectory& trajectory, Eigen::Vector3d& dp)
+{
+  for (double beta = 0.25; beta <= 1.5; beta += 0.25)
+  {
+    // std::cout << "beta: " << beta << std::endl;
+    if (beta == 1.0f)
+    {
+      continue;
     }
+    Eigen::Vector3d dp_ = beta * dp;
+    MotionModelDiffDrive::Trajectory trajectory_;
+    MotionModelDiffDrive::ControlParams output_ = output;
+    output_.omega.km += dp_(0);
+    output_.omega.kf += dp_(1);
+    output_.omega.sf += dp_(2);
+    // std::cout << "dp_: " << dp_.transpose() << std::endl;
+    // output_.omega.km = std::min(std::max(output_.omega.km, -MAX_YAWRATE),
+    // MAX_YAWRATE); output_.omega.kf = std::min(std::max(output_.omega.kf,
+    // -MAX_YAWRATE), MAX_YAWRATE); std::cout << "output: " << output_.omega.km
+    // << ", " << output_.omega.kf << ", " << output_.omega.sf << std::endl;
+    model.generate_trajectory(dt, output_, trajectory_);
+    // std::cout << "size: " << trajectory_.trajectory.size() << std::endl;
+    if (trajectory_.trajectory.size() <= 1)
+    {
+      // std::cout << "failed to generate trajecotry!!!" << std::endl;
+      continue;
+    }
+    Eigen::Vector3d cost_ = goal - trajectory_.trajectory.back();
+    // std::cout << cost_.norm() << " vs " << cost.norm() << std::endl;
+    if (cost_.norm() < cost.norm())
+    {
+      cost = cost_;
+      output = output_;
+      trajectory = trajectory_;
+      dp = dp_;
+      // std::cout << "updated!!!" << std::endl;
+    }
+    else
+    {
+      // std::cout << "not updated!!!" << std::endl;
+    }
+    if (cost.norm() < tolerance)
+    {
+      return;
+    }
+  }
 }
